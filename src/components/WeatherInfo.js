6a1b8fde6d70c5450f39123ec07f6e6972ab8fe1
izal <izal@gmail.com>
2024-06-22 22:01:03 +0700

    Project Weather App

---
+++ src/components/WeatherInfo.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image } from 'react-native'
+
+const WeatherInfo = ({ weatherData }) => {
+    const {
+        name,
+        main: { temp },
+        weather,
+        visibility,
+        wind: { speed },
+      } = weatherData
+  return (
+    <View style={styles.marginTop20}>
+      <Text style={styles.text}>The weather of {name}</Text>
+      <Text style={[styles.temperature, styles.marginTop20]}>{temp} °C</Text>
+      <View style={[styles.rowContainer, styles.marginTop20]}>
+        <Image
+          source={{ uri: 'https://openweathermap.org/img/w/04d.png' }}
+          style={styles.weatherIcon}
+        />
+        <Text style={[styles.text, styles.bold]}>{weather[0].main}</Text>
+      </View>
+      <Text style={styles.text}>{weather[0].description}</Text>
+      <View style={[styles.rowContainer, styles.marginTop20]}>
+        <Text style={[styles.text, styles.bold]}>Visibility :</Text>
+        <Text style={[styles.text, styles.marginLeft15]}>{visibility} km</Text>
+      </View>
+      <View style={[styles.rowContainer, styles.marginTop20]}>
+        <Text style={[styles.text, styles.bold]}>Wind Speed :</Text>
+        <Text style={[styles.text, styles.marginLeft15]}>{speed} m/s</Text>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  marginTop20: {
+    marginTop: 20,
+  },
+  marginLeft15: {
+    marginLeft: 15,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  bold: {
+    fontWeight: '700',
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  temperature: {
+    fontWeight: '700',
+    fontSize: 80,
+    textAlign: 'center',
+  },
+  weatherIcon: {
+    width: 50,
+    height: 50,
+  },
+})
+
+export default WeatherInfo
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+export const API_KEY = 'f3a8f74bb474ee8c59074f4b4d302017';
